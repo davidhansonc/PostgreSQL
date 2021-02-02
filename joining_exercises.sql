28eb01f7e193e810ee359673b60ce5c37ad63946
David Hanson <davidhanson.c@gmail.com>
2021-02-02 15:02:32 -0700

    updated one of the solutions

--- joining_exercises.sql
+++ joining_exercises.sql
@@ -6,10 +6,10 @@
             Oregon (OR) state
 * ordered by orderid
 */
-SELECT o.orderid, o.totalamount, CONCAT(c.firstname, ' ', c.lastname) AS "Name", c.state FROM orders AS o
-INNER JOIN customers AS c ON o.customerid = c.customerid
-    WHERE c.state IN ('OH', 'OH', 'NY')
-ORDER BY orderid;
+-- SELECT o.orderid, o.totalamount, CONCAT(c.firstname, ' ', c.lastname) AS "Name", c.state FROM orders AS o
+-- INNER JOIN customers AS c ON o.customerid = c.customerid
+--     WHERE c.state IN ('OH', 'OH', 'NY')
+-- ORDER BY orderid;
 
 
 /*
@@ -18,7 +18,7 @@ ORDER BY orderid;
 * Question: Show me the inventory for each product
 */
 SELECT p.prod_id, i.quan_in_stock FROM products as p
-INNER JOIN inventory AS i ON p.prod_id = i.prod_id
+INNER JOIN inventory AS i USING(prod_id)
 ORDER BY p.prod_id;
 
 
@@ -27,7 +27,7 @@ ORDER BY p.prod_id;
 * Table: employees
 * Question: Show me for each employee which department they work in
 */
-SELECT a.emp_no, CONCAT(first_name, ' ', last_name) as Name, c.dept_name FROM employees AS a
-INNER JOIN dept_emp as b ON a.emp_no = b.emp_no
-INNER JOIN departments as c ON b.dept_no = c.dept_no
-ORDER BY a.emp_no;
+-- SELECT a.emp_no, CONCAT(first_name, ' ', last_name) as Name, c.dept_name FROM employees AS a
+-- INNER JOIN dept_emp as b ON a.emp_no = b.emp_no
+-- INNER JOIN departments as c ON b.dept_no = c.dept_no
+-- ORDER BY a.emp_no;
