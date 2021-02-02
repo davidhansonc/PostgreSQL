0451e9741bd43baa03c1ca388e73c5348416eb62
David Hanson <davidhanson.c@gmail.com>
2021-02-02 10:02:40 -0700

    table joining practice update

--- table_joining.sql
+++ table_joining.sql
@@ -1,4 +1,14 @@
 -- Selects all salary listings for each employee.
+/*
 SELECT a.emp_no, concat(a.first_name, ' ', a.last_name) AS "name", b.salary
-FROM employees as a
-INNER JOIN salaries as b ON b.emp_no = a.emp_no;
+FROM employees AS a
+INNER JOIN salaries as b ON b.emp_no = a.emp_no
+ORDER BY a.emp_no;
+*/
+
+-- Select salaries that lead to a new title
+SELECT a.emp_no, b.salary, c.title
+FROM employees AS a
+INNER JOIN salaries as b ON b.emp_no = a.emp_no
+INNER JOIN titles as c ON c.emp_no = a.emp_no
+ORDER BY a.emp_no;
