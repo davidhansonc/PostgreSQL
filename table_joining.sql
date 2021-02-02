b274c148093e5744ccdc4ed4e9f18305fb6f1ffa
David Hanson <davidhanson.c@gmail.com>
2021-02-02 14:02:15 -0700

    updated select statement for joining salary with title change: combine inner join with outer join

--- table_joining.sql
+++ table_joining.sql
@@ -7,8 +7,9 @@ ORDER BY a.emp_no;
 */
 
 -- Select salaries that lead to a new title
-SELECT a.emp_no, b.salary, c.title
+SELECT a.emp_no, b.salary, COALESCE(c.title, '-'), b.from_date
 FROM employees AS a
 INNER JOIN salaries as b ON b.emp_no = a.emp_no
-INNER JOIN titles as c ON c.emp_no = a.emp_no
-ORDER BY a.emp_no;
+LEFT JOIN titles as c 
+    ON c.emp_no = a.emp_no AND c.from_date = (b.from_date + INTERVAL '2 days')
+ORDER BY a.emp_no ASC;
