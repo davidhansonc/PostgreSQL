ea139f7e1133348c631f67e4eec5995538e740bf
David Hanson <davidhanson.c@gmail.com>
2021-02-01 17:42:56 -0700

    Update dates.sql

--- dates.sql
+++ dates.sql
@@ -4,8 +4,8 @@
 * Question: Get me all the employees above 60, use the appropriate date functions
 */
 
--- SELECT AGE(birth_date), * FROM employees WHERE EXTRACT(YEAR FROM AGE(birth_date)) >= 60;
--- SELECT AGE(birth_date), * FROM employees WHERE now() - interval '60 years' >= birth_date;
+SELECT AGE(birth_date), * FROM employees WHERE EXTRACT(YEAR FROM AGE(birth_date)) >= 60;
+SELECT AGE(birth_date), * FROM employees WHERE now() - interval '60 years' >= birth_date;
 
 /*
 * DB: Employees
@@ -13,7 +13,7 @@
 * Question: How many employees where hired in February?
 */
 
--- SELECT COUNT(emp_no) FROM employees WHERE EXTRACT (MONTH FROM hire_date) = 2;
+SELECT COUNT(emp_no) FROM employees WHERE EXTRACT (MONTH FROM hire_date) = 2;
 
 /*
 * DB: Employees
@@ -21,7 +21,7 @@
 * Question: How many employees were born in november?
 */
 
--- SELECT COUNT(emp_no) FROM employees WHERE EXTRACT (MONTH FROM birth_date) = 11;
+SELECT COUNT(emp_no) FROM employees WHERE EXTRACT (MONTH FROM birth_date) = 11;
 
 /*
 * DB: Employees
@@ -29,7 +29,7 @@
 * Question: Who is the oldest employee? (Use the analytical function MAX)
 */
 
--- SELECT MAX(AGE(birth_date)) FROM employees WHERE;
+SELECT MAX(AGE(birth_date)) FROM employees WHERE;
 
 /*
 * DB: Store
@@ -37,4 +37,4 @@
 * Question: How many orders were made in January 2004?
 */
 
- SELECT COUNT(orderid) FROM orders WHERE DATE_TRUNC('month', orderdate) = date('2004-01-01');
+SELECT COUNT(orderid) FROM orders WHERE DATE_TRUNC('month', orderdate) = date('2004-01-01');
