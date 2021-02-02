6394c352c3bc372df11292d5a95347d71f2d8475
David Hanson <davidhanson.c@gmail.com>
2021-02-01 18:01:57 -0700

    order by keyword exercise

---
+++ order_by_kw.sql
@@ -0,0 +1,23 @@
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Sort employees by first name ascending and last name descending
+*/
+SELECT first_name, last_name FROM employees ORDER BY first_name ASC, last_name DESC;
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Sort employees by age
+*/
+SELECT * FROM employees ORDER BY AGE(birth_date);
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Sort employees who's name starts with a "k" by hire_date
+*/
+SELECT * FROM employees
+WHERE first_name ILIKE 'k%'
+ORDER BY hire_date ASC;
